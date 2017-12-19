233912bc7cbcdb9711467b033083504dd1ab22b8
Marcin Morawski <marcin@morawskim.pl>
2017-12-19 16:12:45 +0100

    Add some restriction to bash completion

--- completions/bash_completion.sh
+++ completions/bash_completion.sh
@@ -5,18 +5,30 @@ _faker.php()
     local prev="${COMP_WORDS[COMP_CWORD-1]}"
     local line=${COMP_LINE}
     local cmd="${1##*/}"
+    local subcommand=${COMP_WORDS[1]}
 
     local PARAM_LOCALE=''
+    local subcommands='list-types'
     local opts="-l --locale -s --seed -p --pattern -d --delimiter -e --enclosure -E --escape -f --format -c --count"
+
+    if [[ ${COMP_CWORD} == 1 ]]; then
+        local types=$(faker.php list-types ${PARAM_LOCALE} 2> /dev/null | awk '{print $1}')
+        COMPREPLY=( $(compgen -W "${subcommands} ${types} ${opts}" -- ${cur}) )
+        return 0
+    fi
+
+    if [[ ${subcommand} != -* ]]; then
+        return 0
+    fi
 
     for index in "${!COMP_WORDS[@]}"; do
         local arg=${COMP_WORDS[$index]}
 
         case "$arg" in
-            -l) PARAM_LOCALE=" -l ${COMP_WORDS[$index+1]}"
+            -l) [[ -n "${COMP_WORDS[$index+1]}" ]] && PARAM_LOCALE=" -l ${COMP_WORDS[$index+1]}"
                 break
                 ;;
-            --locale) PARAM_LOCALE=" -l ${COMP_WORDS[$index+1]}"
+            --locale) [[ -n ${COMP_WORDS[$index+1]} ]] && PARAM_LOCALE=" -l ${COMP_WORDS[$index+1]}"
                 break
                 ;;
         esac
